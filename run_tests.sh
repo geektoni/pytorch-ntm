2b0ec67ec8717c1403a3eb9d167f453be4425c1e
Giovanni De Toni <giovanni.det@gmail.com>
2019-05-03 10:38:54 +0200

    Fix run_tests.sh (from seq_max_length to seq_max_len)

--- run_tests.sh
+++ run_tests.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 100000 -psequence_width=8 -psequence_max_length=20 -pnum_batches=1000000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 100000 -psequence_width=8 -psequence_max_len=20 -pnum_batches=1000000 --checkpoint-path ./results/priority-sort/
 
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=8 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=12 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=16 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=20 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=24 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=28 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
-python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_length=30 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=8 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=12 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=16 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=20 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=24 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=28 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
+python3 ./train.py --seed 42 --task priority-sort --checkpoint-interval 10000 -psequence_width=1 -psequence_max_len=30 -pnum_batches=100000 --checkpoint-path ./results/priority-sort/
